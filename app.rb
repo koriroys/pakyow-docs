f73bd12262f4bf3892a47663c4b2fb678262c5a0
Bryan Powell <bryan@metabahn.com>
2013-02-24 20:51:01 -0600

    styling and backend code cleanup

--- app.rb
+++ app.rb
@@ -30,56 +30,49 @@ module PakyowApplication
     core do
       fn(:navigation) {
         categories = Docs.find_categories
-        presenter.view.scope('nav-category').repeat(categories) {|context, category|
-          context.bind(category)
-          #TODO remove once binder is working
-          context.prop('name_link').attributes.href = "/categories/#{category[:nice_name]}"
-          context.prop('name_link').content = category[:name]
-
+        view.container(:nav).scope(:category).apply(categories) {|context, category|
           topics = Docs.find_topics(category[:category])
-          context.scope('nav-topic').repeat(topics) {|t_context, topic|
-            t_context.bind(topic)
-            #TODO remove once binder is working
-            t_context.prop('name_link').attributes.href = "/categories/#{category[:nice_name]}##{topic[:nice_name]}"
-            t_context.prop('name_link').content = topic[:name]
-          }
+          context.scope(:topic).apply(topics)
         }
       }
 
-      get('/', after: fn(:navigation)) {
-        #TODO
-      }
+      get('/:name', :doc, after: fn(:navigation)) {
+        name = params[:name]
+        
+        if name && !name.empty?
+          presenter.view_path = '/doc'
 
-      get('/categories/:name', after: fn(:navigation)) {
-        #TODO not really sure why I have to set this?
-        presenter.view = View.new('pakyow.html').compile('categories')
+          if category = Docs.find(params[:name]).first
+            topics = Docs.find_topics(category[:category])
 
-        if category = Docs.find(request.params[:name]).first
-          topics = Docs.find_topics(category[:category])
-
-          presenter.view.scope('category').bind(category)
-          presenter.view.scope('topic').repeat(topics) {|context, topic|
-            context.prop('name').attributes.id = topic[:nice_name]
-            context.bind(topic)
-          }
+            view.container(:main).with { |view|
+              view.scope(:category).bind(category)
+              
+              view.scope(:topic).apply(topics) {|context, topic|
+                context.prop('name').attributes.id = topic[:nice_name]
+              }
+            }
+          else
+            app.handle 404
+          end
         else
-          #TODO not_found
+          # show index
+          presenter.view_path = '/'
         end
       }
     end
 
-    #TODO can't get this to work. probably doing something dumb
     presenter do
-      scope('nav-category') {
+      scope(:category) {
         binding(:name_link) {
           {
-            href: "/categories/#{bindable[:nice_name]}",
+            href: router.path(:doc, { name: bindable[:nice_name] }),
             content: bindable[:name]
           }
         }
       }
 
-      scope('nav-topic') {
+      scope(:topic) {
         binding(:name_link) {
           {
             href: "##{bindable[:nice_name]}",
