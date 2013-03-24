3771d6b513bb70bf0a1b9ba74fea24ee0ce38146
Bryan Powell <bryan@metabahn.com>
2013-03-24 10:38:56 -0500

    Keep formatting from failing when no lexer

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -6,10 +6,13 @@ class Formatter
     doc.css('pre').each do |pre|
       code = pre.css('code').first
       
-      lexer_name = code.text.split("\n")[0].gsub(':', '')
-      code.inner_html = Pygments.highlight(code.inner_html.split("\n")[1..-1].join("\n").strip.gsub(/&lt;|&gt;/) {|s| {'&lt;' => '<','&gt;' => '>'}[s]}, :lexer => lexer_name)
-      code.set_attribute('class', lexer_name)
-      pre.replace(code)
+      begin
+        lexer_name = code.text.split("\n")[0].gsub(':', '')
+        code.inner_html = Pygments.highlight(code.inner_html.split("\n")[1..-1].join("\n").strip.gsub(/&lt;|&gt;/) {|s| {'&lt;' => '<','&gt;' => '>'}[s]}, :lexer => lexer_name)
+        code.set_attribute('class', lexer_name)
+        pre.replace(code)
+      rescue
+      end
       
     end
     
