5cfeba16eebd00d0973bbab577e9f9bf60e04713
Bryan Powell <bryan@metabahn.com>
2013-06-12 17:07:49 -0500

    upgrade pakyow

--- app.rb
+++ app.rb
@@ -11,103 +11,101 @@ Sass::Plugin.options[:css_location] = './public/css'
 
 require 'pp'
 
-module PakyowApplication
-  class Application < Pakyow::Application
-    config.app.default_environment = :development
+Pakyow::App.define do
+  config.app.default_environment = :development
 
-    configure(:development) do
-      $stdout.sync = true
-      $docs_path = 'docs'
-    end
+  configure(:development) do
+    $stdout.sync = true
+    $docs_path = 'docs'
+  end
 
-    configure(:test) do
-      $docs_path = 'test/docs'
-    end
+  configure(:test) do
+    $docs_path = 'test/docs'
+  end
 
-    configure(:production) do
-      $stdout.sync = true
-      $docs_path = 'docs'
-    end
+  configure(:production) do
+    $stdout.sync = true
+    $docs_path = 'docs'
+  end
 
-    configure(:prototype) do
-      app.ignore_routes = true
-    end
+  configure(:prototype) do
+    app.ignore_routes = true
+  end
 
-    core do
-      fn(:navigation) {
-        categories = Docs.find_categories
-        view.container(:nav).scope(:category).apply(categories) {|context, category|
+  routes do
+    fn(:navigation) {
+      categories = Docs.find_categories
+      view.container(:nav).scope(:category).apply(categories) {|context, category|
+        topics = Docs.find_topics(category[:category])
+
+        # add default overview topic
+        topics.unshift({
+          category: category[:category],
+          category_nice_name: category[:nice_name],
+          topic: '0000',
+          nice_name: 'overview',
+          name: 'Overview'
+        })
+
+        context.scope(:topic).apply(topics)
+      }
+    }
+
+    get('/:name', :doc, after: fn(:navigation)) {
+      name = params[:name]
+      
+      if name && !name.empty?
+        presenter.view_path = '/doc'
+
+        if category = Docs.find(params[:name]).first
           topics = Docs.find_topics(category[:category])
 
-          # add default overview topic
-          topics.unshift({
-            category: category[:category],
-            category_nice_name: category[:nice_name],
-            topic: '0000',
-            nice_name: 'overview',
-            name: 'Overview'
-          })
-
-          context.scope(:topic).apply(topics)
-        }
-      }
-
-      get('/:name', :doc, after: fn(:navigation)) {
-        name = params[:name]
-        
-        if name && !name.empty?
-          presenter.view_path = '/doc'
-
-          if category = Docs.find(params[:name]).first
-            topics = Docs.find_topics(category[:category])
-
-            view.container(:main).with { |view|
-              view.scope(:category).bind(category)
-              
-              view.scope(:topic).apply(topics) {|context, topic|
-                context.prop('name').attributes.id = topic[:nice_name]
-              }
+          view.container(:main).with { |view|
+            view.scope(:category).bind(category)
+            
+            view.scope(:topic).apply(topics) {|context, topic|
+              context.prop('name').attributes.id = topic[:nice_name]
             }
-          else
-            app.handle 404
-          end
+          }
         else
-          # show index
-          presenter.view_path = '/'
+          app.handle 404
         end
-      }
-    end
-
-    presenter do
-      scope(:category) {
-        binding(:name_link) {
-          {
-            href: router.path(:doc, { name: bindable[:nice_name] }),
-            content: bindable[:name]
-          }
-        }
-
-        binding(:formatted_body) {
-          Formatter.format(bindable[:body])
-        }
-      }
-
-      scope(:topic) {
-        binding(:name_link) {
-          {
-            href: router.path(:doc, { name: bindable[:category_nice_name] }) + "##{bindable[:nice_name]}",
-            content: bindable[:name]
-          }
-        }
-
-        binding(:formatted_body) {
-          Formatter.format(bindable[:body])
-        }
-      }
-    end
-
-    middleware {
-      use Sass::Plugin::Rack
+      else
+        # show index
+        presenter.view_path = '/'
+      end
     }
   end
+
+  bindings do
+    scope(:category) {
+      binding(:name_link) {
+        {
+          href: router.path(:doc, { name: bindable[:nice_name] }),
+          content: bindable[:name]
+        }
+      }
+
+      binding(:formatted_body) {
+        Formatter.format(bindable[:body])
+      }
+    }
+
+    scope(:topic) {
+      binding(:name_link) {
+        {
+          href: router.path(:doc, { name: bindable[:category_nice_name] }) + "##{bindable[:nice_name]}",
+          content: bindable[:name]
+        }
+      }
+
+      binding(:formatted_body) {
+        Formatter.format(bindable[:body])
+      }
+    }
+  end
+
+  middleware {
+    use Sass::Plugin::Rack
+  }
 end
