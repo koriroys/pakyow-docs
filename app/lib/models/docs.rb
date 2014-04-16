ee6dac244ae64c3f5d479a39d35c01355a377f4f
Kori Roys <kori@koriroys.com>
2014-04-16 03:02:43 -0500

    extract category from Docs

--- app/lib/models/docs.rb
+++ app/lib/models/docs.rb
@@ -1,41 +1,70 @@
 require 'pathname'
 
-class Docs
-
+class Category
   MATCHER = /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
 
+  def initialize(category)
+    @category_name = category
+    @result = { nice_name: @category_name, topics: [] }
+  end
+
+  def topics
+    topic_files_paths = Dir.glob(File.join("docs", @category_name, '*.md'))
+
+    topic_files_paths.each do |topic_file_path|
+      topic = parse_file(topic_file_path)
+      topic[:category_nice_name] = @category_name
+
+      if topic[:nice_name] == '_overview'
+        @result[:name] = topic[:name]
+        topic[:nice_name] = 'overview'
+        @result[:topics].unshift(topic)
+      else
+        @result[:topics] << topic
+      end
+
+      sort!
+    end
+    @result
+  end
+
+  private
+
+  def parse_file(path)
+    data = {}
+    pn = Pathname.new(path)
+    raw = pn.read
+    options = YAML.load(raw.match(MATCHER).to_s)
+
+    data[:nice_name] = pn.basename(".*").to_s
+    data[:name] = options['name']
+    data[:body] = raw.gsub(MATCHER, '')
+
+    data
+  end
+
+  def topic_order
+    @topic_order ||= YAML.load(File.read(path))
+  end
+
+  def path
+    @path ||= File.join($docs_path, @category_name, '_order.yaml')
+  end
+
+  def sort!
+    @result[:topics].sort! { |a,b|
+      i_a = topic_order.index(a[:nice_name])
+      i_b = topic_order.index(b[:nice_name])
+      i_a.to_i <=> i_b.to_i
+    }
+  end
+end
+
+class Docs
   class << self
     def load(categories)
-      @results = []
-      categories.each { |category|
-        result = { nice_name: category, topics: [] }
-
-        Dir.glob(File.join($docs_path, category, '*.md')).each do |path|
-          topic = parse_file(path)
-          topic[:category_nice_name] = category
-
-          if topic[:nice_name] == '_overview'
-            result[:name] = topic[:name]
-            topic[:nice_name] = 'overview'
-
-            result[:topics].unshift(topic)
-          else
-            result[:topics] << topic
-          end
-        end
-
-        topic_order = YAML.load(
-          File.read(
-            File.join($docs_path, category, '_order.yaml')
-          )
-        )
-        result[:topics].sort! { |a,b|
-          i_a = topic_order.index(a[:nice_name])
-          i_b = topic_order.index(b[:nice_name])
-
-          i_a.to_i <=> i_b.to_i
-        }
-        @results << result
+      @results = categories.each.with_object([]) { |category, topics|
+        topics << Category.new(category).topics
       }
     end
 
@@ -55,21 +84,5 @@ class Docs
       }
     end
 
-    private
-
-    def parse_file(path)
-      data = {}
-      pn = Pathname.new(path)
-
-      data[:nice_name] = pn.basename(".*").to_s
-
-      raw = File.read(path)
-
-      options = YAML.load(raw.match(MATCHER).to_s)
-      data[:name] = options['name']
-
-      data[:body] = raw.gsub(MATCHER, '')
-      data
-    end
   end
 end
