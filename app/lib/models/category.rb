1310eb1eb79a5bf132b25f93976778bd4cbcfc7b
Kori Roys <kori@koriroys.com>
2014-04-25 15:17:04 -0500

    extract file parsing out of category object

--- app/lib/models/category.rb
+++ app/lib/models/category.rb
@@ -1,44 +1,15 @@
 class Category
-  attr_reader :slug, :name, :overview, :topics
+  attr_reader :slug, :name, :overview
+  attr_accessor :topics
 
   def initialize(slug)
     @slug = slug
-    @name = nil
     @topics = []
-    @overview = nil
-  end
-
-  def process_topics!
-    topic_files_paths = Dir.glob(File.join("docs", @slug, '*.md'))
-    topic_files_paths.each do |topic_file_path|
-      topic_parser = TopicParser.new(topic_file_path, slug)
-      if category_heading?(topic_parser.slug)
-        set_category_attributes(topic_parser.name, topic_parser.body)
-      else
-        topic = Topic.new(topic_parser.to_hash)
-        append_topic_to_topics_list(topic)
-      end
-    end
-    sort!
-  end
-
-  private
-
-  def category_heading?(slug)
-    slug == "_overview"
   end
 
   def set_category_attributes(name, overview)
     @name = name
     @overview = overview
-  end
-
-  def append_topic_to_topics_list(topic)
-    @topics << topic
-  end
-
-  def sort!
-    @topics.sort_by!(&:order)
   end
 end
 
--- app/lib/models/docs.rb
+++ app/lib/models/docs.rb
@@ -7,9 +7,28 @@ class Docs
     def load(categories_slugs)
       @categories = categories_slugs.map { |category_slug|
         category = Category.new(category_slug)
-        category.process_topics!
+        category.topics = parse_topics(category, category_slug)
         category
       }
+    end
+
+    def parse_topics(category, category_slug)
+      topics_file_paths = Dir.glob(File.join("docs", category_slug, '*.md'))
+
+      topics = topics_file_paths.each.with_object([]) do |topic_file_path, topics|
+        topic_parser = TopicParser.new(topic_file_path, category_slug)
+        if category_heading?(topic_parser.slug)
+          category.set_category_attributes(topic_parser.name, topic_parser.body)
+        else
+          topics << Topic.new(topic_parser.to_hash)
+        end
+      end
+
+      topics.sort_by(&:order)
+    end
+
+    def category_heading?(slug)
+      slug == "_overview"
     end
 
     def find(category_slug)
